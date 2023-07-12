205f3bdc43661c9882a82927858d197ba2edc810
pranay7568877 <139395449+pranay7568877@users.noreply.github.com>
2023-07-12 22:15:44 +0100

    Create styles.js
    Adding Styles.js

---
+++ styles.js
@@ -0,0 +1,29 @@
+body {
+  font-family: Arial, sans-serif;
+  margin: 20px;
+}
+
+h1 {
+  text-align: center;
+}
+
+.container {
+  margin-top: 20px;
+}
+
+textarea {
+  width: 100%;
+  height: 100px;
+  resize: vertical;
+}
+
+button {
+  margin-top: 10px;
+}
+
+.ticket {
+  margin-bottom: 10px;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
